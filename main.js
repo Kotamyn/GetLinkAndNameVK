0b353cdf9ec273e0eb06e549f98d9271db905424
Kotamyn <67268519+Kotamyn@users.noreply.github.com>
2023-09-07 17:26:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-window.scrollTo(0,document.body.scrollHeight); // Прокрутка до конца страницы
-const allParagraphs = [...document.querySelectorAll('.friends_field_title a:not([href="https://vk.com/faq20572"])')];
-const __data = allParagraphs.map(p => `https://vk.com/${p.getAttribute('href')} - ${p.textContent}`);
-console.log(__data);
+async function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+}
+
+async function scroll_page_end(){
+    const friends = document.querySelector('li[id="friends_tab_all"] span[class="ui_tab_count_new"]').innerText; // Получаем количество друзей
+    const count_scroll = Math.floor(parseInt(friends) / 15);
+    for (let i = 0; i < count_scroll; i++){
+        window.scrollTo(0,document.body.scrollHeight); // Прокрутка страницы
+        await sleep(1200)
+    }
+    return true
+}
+let go = await scroll_page_end();
+if (go) {
+    const users = [...document.querySelectorAll('.friends_field_title a:not([href="https://vk.com/faq20572"])')];
+    const user_info = users.map(p => `https://vk.com/${p.getAttribute('href')} - ${p.textContent}`); // Берем ссылку и имя/фамилия
+    console.log(user_info);
+}
