cc54e3a5d57473913dab9ff7e449a258575ff144
Kotamyn <67268519+Kotamyn@users.noreply.github.com>
2023-09-24 21:29:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 async function sleep(ms) {
-  return new Promise(
-    resolve => setTimeout(resolve, ms)
-  );
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function scroll_page_end(){
+async function printInfo(){
+  const userInfo = [...document.querySelectorAll('.friends_field_title a:not([href="https://vk.com/faq20572"])')]
+    .map(p => `https://vk.com/${p.getAttribute('href')} - ${p.textContent}`); // Берем ссылку и имя/фамилия
+  console.log(userInfo);
+}
+  
+async function scrollPageEnd(){
     const friends = document.querySelector('li[id="friends_tab_all"] span[class="ui_tab_count_new"]').innerText; // Получаем количество друзей
-    const count_scroll = Math.floor(parseInt(friends) / 15);
-    for (let i = 0; i < count_scroll; i++){
+    const countScroll = Math.floor(parseInt(friends) / 15);
+    for (let i = 0; i < countScroll; i++){
         window.scrollTo(0,document.body.scrollHeight); // Прокрутка страницы
-        await sleep(1200)
+        await sleep(1200) // Пауза для загрузки новых друзей
     }
-    return true
 }
-let go = await scroll_page_end();
-if (go) {
-    const users = [...document.querySelectorAll('.friends_field_title a:not([href="https://vk.com/faq20572"])')];
-    const user_info = users.map(p => `https://vk.com/${p.getAttribute('href')} - ${p.textContent}`); // Берем ссылку и имя/фамилия
-    console.log(user_info);
+
+async function main(){
+  await scrollPageEnd();
+  await printInfo();
 }
+
+await main();
